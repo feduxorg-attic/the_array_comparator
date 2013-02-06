c9724cc946f45ba889a905ae50b009433b603f76
Max Meyer <dev@fedux.org>
2013-02-06 14:26:22 +0100

    added specs for sample

--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -2,10 +2,23 @@
 require 'spec_helper'
 
 describe Sample do
-  it "takes data, keywords, exceptions, tags" do
+  it "takes nothing by default" do
     expect {
       Sample.new()
-    }.to raise_error
+    }.to_not raise_error
+  end
 
+  it "takes data, keywords, exceptions, tags" do
+      data = %w{ data }
+      keywords = %w{ keywords }
+      exceptions = %w{ exceptions }
+      tag = 'this is a tag'
+
+      sample = Sample.new(keywords, data, exceptions,tag)
+
+      expect(sample.data).to eq(data)
+      expect(sample.keywords).to eq(keywords)
+      expect(sample.exceptions).to eq(exceptions)
+      expect(sample.tag).to eq(tag)
   end
 end
