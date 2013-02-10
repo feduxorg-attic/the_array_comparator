8b7d6b0f47817f3bcaaf16eb1f149f34630710ee
Max Meyer <dev@fedux.org>
2013-02-10 17:42:39 +0100

    introduce new result class

---
+++ spec/result_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+#describe Result do
+#  it "" do
+#    #result = Result.new(true, 
+#  end
+#
+#  it "returns the failed check" do
+#  end
+#
+#  it "is true if the check is successfull" do
+#
+#  end
+#end
