be3c34ef38464645d1751ca053b9043530af2d8a
Max Meyer <dev@fedux.org>
2013-02-10 19:26:23 +0100

    result should be encapsulated

--- lib/the_array_comparator/result.rb
+++ lib/the_array_comparator/result.rb
@@ -4,6 +4,18 @@
 module TheArrayComparator
   # the check result
   class Result
+    def initialize(sample=nil)
+      @sample = sample
+    end
+
+    def from_check
+      return true if @sample.blank?
+      false
+    end
+
+    def failed_sample
+      @sample
+    end
 
   end
 end
--- spec/result_spec.rb
+++ spec/result_spec.rb
@@ -1,14 +1,31 @@
 require 'spec_helper'
 
-#describe Result do
-#  it "" do
-#    #result = Result.new(true, 
-#  end
-#
-#  it "returns the failed check" do
-#  end
-#
-#  it "is true if the check is successfull" do
-#
-#  end
-#end
+describe Result do
+  sample_klass = Class.new do
+    attr_accessor :data, :keywords, :exceptions, :tag
+    
+    def initialize(data,keywords,exceptions,tag)
+      @data = data
+      @keywords = keywords
+      @exceptions = exceptions
+      @tag = tag
+    end
+  end
+
+  it "returns true if _no_ sample is defined" do
+    result = Result.new()
+    expect(result.from_check).to eq(true)
+  end
+
+  it "returns false if sample is defined" do
+    data = %w{ data }
+    keywords = %w{ keywords }
+    exceptions = %w{ exceptions }
+    tag = 'this is a tag'
+    sample = sample_klass.new(data,keywords,exceptions,tag)
+
+    result = Result.new(sample)
+    expect(result.from_check).to eq(false)
+  end
+
+end
