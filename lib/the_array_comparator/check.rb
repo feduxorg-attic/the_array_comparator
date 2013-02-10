e80a00b21cfd1df70632a7179d83312a08a51df4
Max Meyer <dev@fedux.org>
2013-02-10 17:42:20 +0100

    introduce new check class

---
+++ lib/the_array_comparator/check.rb
@@ -0,0 +1,9 @@
+#encoding: utf-8
+
+# the main module
+module TheArrayComparator
+  # the check result
+  class Check
+
+  end
+end
---
+++ spec/check_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+#describe Check do
+#
+#  it "task a sample and a comparator" do
+#    #check = Check.new(comparator,sample)
+#  end
+#
+#  it "is true if the check is successfull" do
+#
+#  end
+#end
